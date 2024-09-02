29b2e485ddac228d8c49a12a723423ff857f3ac5
Tangu12 <justintang99@gmail.com>
2024-09-02 21:59:46 +0800

    First Draft

--- src/TeamProj1/InsertionSort.java
+++ src/TeamProj1/InsertionSort.java
@@ -1,16 +1,16 @@
 package TeamProj1;
     public class InsertionSort {
 
+    }
 
-
-        public static void insertionsort(int[] inputArray) {
-            for (int i = 1; i < inputArray.length; i++) {
-                for (int j = i; j > 0; j--) {
-                    if (inputArray[j] > inputArray[j-1]){
-                        int temp = inputArray[j];
-                        inputArray[j] = inputArray[j-1];
-                        inputArray[j-1] = temp;
-                    }
-                    hybridComparisions++;
-                }
+//        public static void insertionsort(int[] inputArray) {
+//            for (int i = 1; i < inputArray.length; i++) {
+//                for (int j = i; j > 0; j--) {
+//                    if (inputArray[j] > inputArray[j-1]){
+//                        int temp = inputArray[j];
+//                        inputArray[j] = inputArray[j-1];
+//                        inputArray[j-1] = temp;
+//                    }
+//                    hybridComparisions++;
+//                }
 
